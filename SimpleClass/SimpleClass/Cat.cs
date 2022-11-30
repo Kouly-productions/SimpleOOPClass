805c357c22259552020cf3e4ceafbdc677805433
DayKnight <48128587+Kouly-productions@users.noreply.github.com>
2022-11-30 08:24:59 +0100

    Uploaded project

---
+++ SimpleClass/SimpleClass/Cat.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleClass
+{
+    public class Cat
+    {
+        public Cat(string farve)
+        {
+            Farve = farve;
+        }
+
+        public string Farve { get; set; }
+
+        public string nyFarve()
+        {
+            Farve = "blå";
+            return Farve;
+        }
+    }
+}
---
+++ SimpleClass/SimpleClass/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https:/using System;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace SimpleClass // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat c1 = new Cat("Grå");
+
+            Console.WriteLine(c1.Farve);
+
+            c1.nyFarve();
+
+            Console.WriteLine(c1.Farve);
+        }
+    }
+}
